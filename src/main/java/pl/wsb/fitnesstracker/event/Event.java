a0a5b0d56e5dfd5af1fc08f78b0c54b8fe1cf415
Visu4lny <12573950+Visu4lny@users.noreply.github.com>
2025-11-23 13:43:45 +0100

    Add Training, WorkoutSession, Event, UserEvent entities

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,38 @@
 package pl.wsb.fitnesstracker.event;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
 // TODO: Define the Event entity with appropriate fields and annotations
+@Entity
+@Table(name = "Event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "start_time", nullable = false)
+    private Date startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private Date endTime;
+
+    private String country;
+
+    private String city;
+
 }
--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,45 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.training.api.Training;
 
 // TODO: Define the Event entity with appropriate fields and annotations
+
+@Entity
+@Table(name = "Workout_Session")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class WorkoutSession {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
     private int id;
-    private int trainingId;
+
+    @OneToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training trainingId;
+
+    @Column(nullable = false)
     private String timestamp;
+
+    @Column(name = "start_latitude")
     private double startLatitude;
+
+    @Column(name = "start_longitude")
     private double startLongitude;
+
+    @Column(name = "end_latitude")
     private double endLatitude;
+
+    @Column(name = "end_longitude")
     private double endLongitude;
+
     private double altitude;
 }
